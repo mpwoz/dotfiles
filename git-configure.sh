bf28878f15a570ba9953c96f79574dfb0eafd6f8
Martin Wozniewicz <martin.woz@gmail.com>
2015-11-02 18:41:44 -0800

    Git configuration script

--- git-configure.sh
+++ git-configure.sh
@@ -4,3 +4,5 @@ git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.lg log
+
+git config --global credential.helper 'cache --timeout=10800' # 3h
