69d34b79ff7cf10f8f3fbb424e623d51cbaaadeb
mpwoz <mpwoz@mpwoz-macbookpro.roam.corp.google.com>
2015-12-02 13:09:41 -0800

    more git configuration

--- git-configure.sh
+++ git-configure.sh
@@ -6,3 +6,6 @@ git config --global alias.st status
 git config --global alias.lg log
 
 git config --global credential.helper 'cache --timeout=10800' # 3h
+
+git config --global user.name "Martin Wozniewicz"
+git config --global user.email martin.woz@gmail.com
