b1707194d1e9600f38742cdfcd062b477cf430ce
Martin Wozniewicz <martin.woz@gmail.com>
2016-06-28 16:07:26 -0700

    fix break-taking script to work with cron

--- take-break.sh
+++ take-break.sh
@@ -3,4 +3,5 @@
 # then add this line to run every 20 minutes:
 #     */20 * * * * ~/src/dotfiles/take-break.sh
 
-notify-send \"Take a break!\"
+export DISPLAY=:0
+/usr/bin/notify-send "Take a break!"
