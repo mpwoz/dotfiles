3802f152bbfa35ee4499954e6d431c2a6d3947a1
Martin Wozniewicz <martin.woz@gmail.com>
2026-02-03 11:07:52 -0800

    cleanup

--- cron/README.md
+++
@@ -1,5 +0,0 @@
-Collection of cron jobs to install on new systems.
-
-Installation:
-
-    ~/src/dotfiles/cron/setup.sh
--- cron/journal.sh
+++
@@ -1,3 +0,0 @@
-# TODO: open vimwiki to journal page, with auto-inserted date line
-# See http://www.stochasticgeometry.ie/2012/11/23/vimwiki/
-
