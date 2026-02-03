56f0de060f05dba2829e40b380389c2916215a0d
Martin Wozniewicz <martin.woz@gmail.com>
2026-02-03 11:19:10 -0800

    cleanup 2

--- .xsetwacom.sh
+++
@@ -1,5 +0,0 @@
-# Make sure to allow execution (chmod +x)
-# Then set as a startup application 
-# sh /home/martin/.xsetwacom.sh
-xsetwacom set 'Wacom Intuos3 9x12 stylus' area 0 0 60960 38100
-
--- zshrc
+++
@@ -1,5 +0,0 @@
-# Gets 256 colors to work in screen
-export TERM=xterm-256color
-
-# Right-side of prompt
-export RPROMPT="%{$fg[cyan]%}%~ %{$fg[default]%}"
